1039d5f0ad21e0a58578a218e11b7342563737c0
baekandy1021 <baek.andy1021@gmail.com>
2025-10-11 23:57:29 -0400

    1:55 ish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import (read_video,save_video)
 from trackers import PlayerTracker,BallTracker
+from court_line_detector import CourtLineDetector
+import cv2 
 def main():
     # Read video
     input_video_path = "input_videos/input_video.mp4"
@@ -16,15 +18,27 @@ def main():
                                                     read_from_stub=True, 
                                                     stub_path="tracker_stubs/ball_detection.pkl")
 
-    # Draw output 
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
 
+    # Court Line Detector model 
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
+
+    # Draw output 
 
     ## Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
-    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
+    ## Draw court keypoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames,court_keypoints)
 
-    save_video(video_frames, "output_videos/output_video.avi")
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}", (10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__== "__main__":
     main()
