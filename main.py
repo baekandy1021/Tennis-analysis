c3d40e814e50e40ff1fda5d259934591b4e0c3b9
baekandy1021 <baek.andy1021@gmail.com>
2025-10-12 00:51:13 -0400

    yolo8 works so much better on the ball

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import (read_video,save_video)
 from trackers import PlayerTracker,BallTracker
 from court_line_detector import CourtLineDetector
 import cv2 
+
 def main():
     # Read video
     input_video_path = "input_videos/input_video.mp4"
@@ -9,13 +10,13 @@ def main():
 
     # Detect Players and Ball
     player_tracker = PlayerTracker(model_path='yolov8l')
-    ball_tracker = BallTracker(model_path="models/yolo5_best.pt")
+    ball_tracker = BallTracker(model_path="models/yolo8_best.pt")
     player_detections = player_tracker.detect_frames(video_frames, 
                                                     read_from_stub=True, 
                                                     stub_path="tracker_stubs/player_detection.pkl")
     
     ball_detections = ball_tracker.detect_frames(video_frames, 
-                                                    read_from_stub=True, 
+                                                    read_from_stub=False, 
                                                     stub_path="tracker_stubs/ball_detection.pkl")
 
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
