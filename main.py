e72f948fdf14f1db07358eb603b0f96389fa8ad7
baekandy1021 <baek.andy1021@gmail.com>
2025-10-19 02:41:31 -0400

    3:02:43 added the minicourt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import (read_video,save_video)
 from trackers import PlayerTracker,BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 import cv2 
 
 def main():
@@ -16,7 +17,7 @@ def main():
                                                     stub_path="tracker_stubs/player_detection.pkl")
     
     ball_detections = ball_tracker.detect_frames(video_frames, 
-                                                    read_from_stub=False, 
+                                                    read_from_stub=True, 
                                                     stub_path="tracker_stubs/ball_detection.pkl")
 
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
@@ -26,9 +27,13 @@ def main():
     court_line_detector = CourtLineDetector(court_model_path)
     court_keypoints = court_line_detector.predict(video_frames[0])
 
+    #Choose players
+    player_detections = player_tracker.choose_and_filter_players(court_keypoints,player_detections)
+    
+    # MiniCourt 
+    mini_court = MiniCourt(video_frames[0])
 
     # Draw output 
-
     ## Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
@@ -36,6 +41,9 @@ def main():
     ## Draw court keypoints
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames,court_keypoints)
 
+    #Draw Mini Court 
+    output_video_frames = mini_court.draw_mini_court(output_video_frames)
+    
     ## Draw frame number on top left corner
     for i, frame in enumerate(output_video_frames):
         cv2.putText(frame, f"Frame: {i}", (10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
